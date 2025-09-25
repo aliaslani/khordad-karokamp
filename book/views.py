923e51f8f66e5459849f35a302f77a522c752206
aliaslani <cinortcele1@gmail.com>
2025-09-25 15:11:31 +0330

    session3

--- book/views.py
+++ book/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+
+def home(request):
+    return HttpResponse("<h1 style='color: red; text-align:center;'>Hello</h1>")
